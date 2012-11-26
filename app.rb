6aa172503fc05b1d9cbe0dce10734d9451ad7da9
Ben Cohen <ben@bencohen.net>
2012-11-26 09:26:14 -0600

    bumped hash version

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ get '/weather/:city' do
 end
 
 def image_filename(str)
-  '/' + Digest::SHA1.hexdigest("v2-#{str}") + '.png'
+  '/' + Digest::SHA1.hexdigest("v3-#{str}") + '.png'
 end
 
 class PopenHandler < EM::Connection
